7662ef8a7a5c57e89916ee3287640a5fbda2521b
Andras Kucsma <andras.kucsma@prezi.com>
2018-06-08 19:03:57 +0200

    Fix roman_numeral parsing

--- python/token.py
+++ python/token.py
@@ -3,20 +3,20 @@ import re
 def tokenize(s):
     tokens = []
     regexes = [
-        ('whitespace', re.compile('^ +')),
-        ('number', re.compile('^[0-9]+')),
-        ('operator', re.compile('^[-+/*=]')),
-        ('parentheses', re.compile('^[()]')),
-        ('roman_number', re.compile('^[IVXLCDM]+ ')),
-        ('word', re.compile('^[a-zA-Z]+')),
-        ('questionmark', re.compile('^\?'))
+        ('whitespace', re.compile('^ +'), 0),
+        ('number', re.compile('^[0-9]+'), 0),
+        ('operator', re.compile('^[-+/*=]'), 0),
+        ('parentheses', re.compile('^[()]'), 0),
+        ('roman_number', re.compile('^([IVXLCDM]+) '), 1),
+        ('word', re.compile('^[a-zA-Z]+'), 0),
+        ('questionmark', re.compile('^\?'), 0)
     ]
     while len(s):
         for r in regexes:
             m = r[1].match(s)
             if m:
-                tokens.append((r[0], m.group()))
-                s = s[len(m.group()):]
+                tokens.append((r[0], m.group(r[2])))
+                s = s[len(m.group(r[2])):]
                 continue
 
     return tokens
@@ -29,6 +29,4 @@ def print_tokens(tokens):
         print(t)
 
 if __name__ == '__main__':
-    for t in tokenize('34 - (IV - asdasd?)'):
-        print(t)
-
+    print_tokens(tokenize('34 - (Is IV - asdasd?)'))
